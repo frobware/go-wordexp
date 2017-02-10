a9f9fa93526f53f436e895ab6ed0e709f54d2c40
Andrew McDermott <aim@frobware.com>
2017-02-10 19:24:41 +0000

    add missing file: export_test.go

---
+++ export_test.go
@@ -0,0 +1,8 @@
+package wordexp
+
+var (
+	NewNativeAllocator      = newNativeAllocator
+	NewWordExpander         = newWordExpander
+	NewWordExpansionError   = newWordExpansionError
+	NewOutOfMemoryAllocator = newOutOfMemoryAllocator
+)
