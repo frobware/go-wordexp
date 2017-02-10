3e81927233287d04f563317ff53136f53ded064b
Andrew McDermott <aim@frobware.com>
2017-02-10 19:22:48 +0000

    prefer blackbox testing

--- wordexp_test.go
+++ wordexp_test.go
@@ -1,8 +1,9 @@
-package wordexp
+package wordexp_test
 
 import (
 	"os"
 
+	wordexp "github.com/frobware/go-wordexp"
 	gc "gopkg.in/check.v1"
 )
 
@@ -11,46 +12,46 @@ type WordExpansionSuite struct{}
 var _ = gc.Suite(&WordExpansionSuite{})
 
 func (s *WordExpansionSuite) TestWordExpansionBadCharacterError(c *gc.C) {
-	_, err := Expand(`testdata/|`)
+	_, err := wordexp.Expand(`testdata/|`)
 	c.Check(err, gc.NotNil)
 	c.Assert(err, gc.ErrorMatches, "bad character")
 }
 
 func (s *WordExpansionSuite) TestWordExpansionBadVariableError(c *gc.C) {
-	_, err := Expand(`${TestWordExpansionBadVariableError}`)
+	_, err := wordexp.Expand(`${TestWordExpansionBadVariableError}`)
 	c.Check(err, gc.NotNil)
 	c.Assert(err, gc.ErrorMatches, "bad variable")
 }
 
 func (s *WordExpansionSuite) TestWordExpansionCmdSubstitutionError(c *gc.C) {
-	_, err := Expand(`$(uname -a)`)
+	_, err := wordexp.Expand(`$(uname -a)`)
 	c.Check(err, gc.NotNil)
 	c.Assert(err, gc.ErrorMatches, "command execution not allowed")
 }
 
 func (s *WordExpansionSuite) TestWordExpansionOutOfMemoryError(c *gc.C) {
-	allocator := newOutOfMemoryAllocator()
-	expander := newWordExpander(allocator)
+	allocator := wordexp.NewOutOfMemoryAllocator()
+	expander := wordexp.NewWordExpander(allocator)
 	_, err := expander.Expand("testdata/wordexp/*.cfg")
 	c.Assert(err, gc.NotNil)
 	c.Assert(err, gc.ErrorMatches, "not enough memory to store the result")
 }
 
 func (s *WordExpansionSuite) TestWordExpansionShellSyntaxError(c *gc.C) {
-	_, err := Expand(`testdata/"`)
+	_, err := wordexp.Expand(`testdata/"`)
 	c.Check(err, gc.NotNil)
 	c.Assert(err, gc.ErrorMatches, "shell syntax error in words")
 }
 
 func (s *WordExpansionSuite) TestWordExpansionForNonExistentFiles(c *gc.C) {
-	results, err := Expand("testdata/wordexp/*.nonexistent")
+	results, err := wordexp.Expand("testdata/wordexp/*.nonexistent")
 	c.Assert(err, gc.IsNil)
 	c.Assert(results, gc.HasLen, 1)
 	c.Check(results[0], gc.Equals, "testdata/wordexp/*.nonexistent")
 }
 
 func (s *WordExpansionSuite) TestWordExpansionForExpectedFiles(c *gc.C) {
-	results, err := Expand("testdata/wordexp/*.cfg")
+	results, err := wordexp.Expand("testdata/wordexp/*.cfg")
 	c.Assert(err, gc.IsNil)
 	c.Assert(results, gc.HasLen, 3)
 	c.Check(results[0], gc.Equals, "testdata/wordexp/a.cfg")
@@ -59,19 +60,19 @@ func (s *WordExpansionSuite) TestWordExpansionForExpectedFiles(c *gc.C) {
 }
 
 func (s *WordExpansionSuite) TestWordExpansionNotAnError(c *gc.C) {
-	err := newWordExpansionError(0)
+	err := wordexp.NewWordExpansionError(0)
 	c.Assert(err, gc.NotNil)
 	c.Check(err, gc.ErrorMatches, "not an error")
 }
 
 func (s *WordExpansionSuite) TestWordExpansionUnknownError(c *gc.C) {
-	err := newWordExpansionError(-1)
+	err := wordexp.NewWordExpansionError(-1)
 	c.Assert(err, gc.NotNil)
 	c.Check(err, gc.ErrorMatches, "unrecognised error code")
 }
 
 func (s *WordExpansionSuite) TestWordExpansionNoMatches(c *gc.C) {
-	results, err := Expand("testdata/wordexp/*.go")
+	results, err := wordexp.Expand("testdata/wordexp/*.go")
 	c.Assert(err, gc.IsNil)
 	c.Assert(results, gc.HasLen, 1)
 	c.Assert(results[0], gc.Equals, "testdata/wordexp/*.go")
